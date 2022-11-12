472e645b850237318143819fb521eb2d9d346f72
chaitanya <chaitanya.tata215@gmail.com>
2022-11-12 14:52:32 +0530

    Fix: ScollTop smoothened

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -5,7 +5,7 @@ import { Button } from '@mui/material';
 export default function ScrollTop({parent}) {
     console.log(parent);
   return (
-        <Button onClick={() => parent.current.scrollIntoView()} style={{ borderRadius: "50%", width: "50px", height: "50px", position: "fixed", bottom: "5px", right: "5px", backgroundColor: "grey", zIndex: "1", minWidth: "0" }}>
+        <Button onClick={() => parent.current.scrollIntoView({behavior: "smooth"})} style={{ borderRadius: "50%", width: "50px", height: "50px", position: "fixed", bottom: "5px", right: "50px", backgroundColor: "grey", zIndex: "1", minWidth: "0" }}>
         <KeyboardArrowUpIcon fontSize='large' />
         </Button>
   )
